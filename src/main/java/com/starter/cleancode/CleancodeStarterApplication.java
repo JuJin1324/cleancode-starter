c17c4aa7a032f8007f0ea660c87483234719f3c8
jujin <jujin@ubivelox.com>
2025-04-29 11:06:00 +0900

    feat: Q&A 확인 SpringBoot 프로젝트 추가

---
+++ src/main/java/com/starter/cleancode/CleancodeStarterApplication.java
@@ -0,0 +1,13 @@
+package com.starter.cleancode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleancodeStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleancodeStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/starter/cleancode/CleancodeStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starter.cleancode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleancodeStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
